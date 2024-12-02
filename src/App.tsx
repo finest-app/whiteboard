69c0113348d41a81fefbed54f8c4c6e2df2293a0
Visualizeit <vnyeit@gmail.com>
2024-12-02 18:13:43 +0800

    feat: add throttleit for debouncing local storage persistence in Tldraw app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,84 @@
 import { getAssetUrlsByImport } from '@tldraw/assets/imports.vite'
-import { Tldraw, type TldrawProps } from 'tldraw'
+import { useLayoutEffect, useState } from 'react'
+import throttle from 'throttleit'
+import {
+	createTLStore,
+	DefaultSpinner,
+	getSnapshot,
+	loadSnapshot,
+	Tldraw,
+	type TldrawProps,
+} from 'tldraw'
+
 import 'tldraw/tldraw.css'
 
 const assetUrls = getAssetUrlsByImport((assetUrl) => assetUrl)
 
+const PERSISTENCE_KEY = 'whiteboard-app'
+
+type LoadingState =
+	| { status: 'loading' }
+	| { status: 'ready' }
+	| { status: 'error'; error: string }
+
 const App = () => {
+	const [store] = useState(() => createTLStore())
+
+	const [loadingState, setLoadingState] = useState<LoadingState>({
+		status: 'loading',
+	})
+
 	const tldrawProps: TldrawProps = {
+		store,
 		assetUrls,
 		inferDarkMode: true,
+	}
+
+	useLayoutEffect(() => {
+		setLoadingState({ status: 'loading' })
+
+		// Get persisted data from local storage
+		const persistedSnapshot = localStorage.getItem(PERSISTENCE_KEY)
+
+		if (persistedSnapshot) {
+			try {
+				const snapshot = JSON.parse(persistedSnapshot)
+				loadSnapshot(store, snapshot)
+				setLoadingState({ status: 'ready' })
+			} catch (error: any) {
+				setLoadingState({ status: 'error', error: error.message }) // Something went wrong
+			}
+		} else {
+			setLoadingState({ status: 'ready' }) // Nothing persisted, continue with the empty store
+		}
+
+		// Each time the store changes, run the (debounced) persist function
+		const cleanupFn = store.listen(
+			throttle(() => {
+				const snapshot = getSnapshot(store)
+				localStorage.setItem(PERSISTENCE_KEY, JSON.stringify(snapshot))
+			}, 500),
+		)
+
+		return () => {
+			cleanupFn()
+		}
+	}, [store])
+
+	if (loadingState.status === 'loading') {
+		return (
+			<div className="grid size-full place-items-center">
+				<DefaultSpinner />
+			</div>
+		)
+	}
+
+	if (loadingState.status === 'error') {
+		return (
+			<div className="grid size-full place-items-center">
+				{loadingState.error}
+			</div>
+		)
 	}
 
 	return <Tldraw {...tldrawProps} />
