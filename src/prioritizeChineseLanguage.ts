6f8247e0be5c0c104374cb81a62047efe0073e10
Visualizeit <vnyeit@gmail.com>
2024-12-07 14:43:49 +0800

    feat: prioritize Chinese language in navigator.languages for improved localization

--- src/prioritizeChineseLanguage.ts
+++ src/prioritizeChineseLanguage.ts
@@ -1,9 +1,11 @@
 const languages = window.navigator.languages
 
+const sortedLanguages = languages.some((language) => language.startsWith('zh'))
+	? [...languages].sort((a) => (a.startsWith('zh') ? -1 : 1))
+	: ['zh-CN', ...languages]
+
 Object.defineProperty(window.navigator, 'languages', {
 	get() {
-		return languages.some((language) => language.startsWith('zh'))
-			? [...languages].sort((a) => (a.startsWith('zh') ? -1 : 1))
-			: ['zh-CN', ...languages]
+		return sortedLanguages
 	},
 })
